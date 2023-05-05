a034a6cd1980cf2bcb5136e8e2e79a5bf8d25e98
Carlos Barros <carlosbwebdev@gmail.com>
2023-05-05 12:56:40 +0100

    ...

--- src/components/VimeoVideo.js
+++ src/components/VimeoVideo.js
@@ -1,44 +1,26 @@
-import React, { useState, useEffect } from "react";
-import { useInView } from "react-intersection-observer";
-import { useScroll, motion, useTransform, useMotionValue } from "framer-motion";
+import React from "react";
 import styles from "../styles/components/VimeoVideo.module.css";
-import VisibilitySensor from "react-visibility-sensor";
+import { useInView } from "react-intersection-observer";
 
 const VimeoVideo = ({ videoId }) => {
-  const [inView, setInView] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  // const { ref, inView } = useInView({
-  //   threshold: 0.3,
-  // });
-
-  // useEffect(() => {
-  //   setIsInView(inView);
-  //   setIsPlaying(inView);
-  // }, [inView]);
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
 
   return (
-    <VisibilitySensor
-      partialVisibility={true}
-      minTopValue={50}
-      onChange={(isVisible) => {
-        setInView(isVisible);
-      }}
-    >
-      <div className={styles.videoContainer}>
-        {inView ? (
-          <iframe
-            src={`https://player.vimeo.com/video/${videoId}?autoplay=1&loop=0&autopause=0&muted=1&controls=0&repeat=1`}
-            frameBorder="0"
-            allow="autoplay; picture-in-picture "
-            lowFullScreen
-            allowfullscreen
-          ></iframe>
-        ) : (
-          ""
-        )}
-      </div>
-    </VisibilitySensor>
+    <div ref={ref} className={styles.videoContainer}>
+      {inView ? (
+        <iframe
+          src={`https://player.vimeo.com/video/${videoId}?autoplay=1&loop=0&autopause=0&muted=1&controls=0&repeat=1`}
+          frameBorder="0"
+          allow="autoplay; picture-in-picture "
+          allowFullScreen
+        ></iframe>
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 
